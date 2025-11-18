137228f3b78f0bc9fc394ae0b01701612df2cc08
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2025-11-18 16:41:40 +0900

    update format

--- src/compo_aframe/robotRegistry.js
+++ src/compo_aframe/robotRegistry.js
@@ -5,30 +5,30 @@ AFRAME.registerComponent('robot-registry', {
     this.el.sceneEl.robotRegistryComp = this;
     this.objects = new Map();
   },
-  set: function(id, data) { // data: {el: robotEl, axes: [...axes]}
-    this.objects.set(id, {data: data});
+  set: function (id, data) { // data: {el: robotEl, axes: [...axes]}
+    this.objects.set(id, { data: data });
   },
-  get: function(id) {
+  get: function (id) {
     return this.objects.get(id)?.data;
   },
-  add: function(id, data) {
+  add: function (id, data) {
     console.log('Registry: registry add id:', id);
     if (id) {
       if (this.get(id)) {
-	console.warn('registry add already exist id:', id);
-	Object.assign(this.get(id), data);
-	console.log('registry add data:', this.get(id));
+        console.warn('registry add already exist id:', id);
+        Object.assign(this.get(id), data);
+        console.log('registry add data:', this.get(id));
       } else {
-	this.set(id, data);
+        this.set(id, data);
       }
     } else {
       console.warn('registry add invalid id:', id, ' data:', data);
     }
   },
-  getWhole: function(id) {
+  getWhole: function (id) {
     return this.objects.get(id);
   },
-  enableEventDelivery: function(id, distributor) {
+  enableEventDelivery: function (id, distributor) {
     const listenerEl = this.objects.get(id)?.data?.el;
     if (checkListenerList(listenerEl, distributor)) {
       distributor.listenersList[id] = listenerEl;
@@ -36,26 +36,26 @@ AFRAME.registerComponent('robot-registry', {
       console.log('enable listening event by', id);
     }
   },
-  disableEventDelivery: function(id, distributor) {
+  disableEventDelivery: function (id, distributor) {
     const listenerEl = this.objects.get(id)?.data?.el;
     if (checkListenerList(listenerEl, distributor)) {
       delete distributor.listenersList[id];
       if (listenerEl.shouldListenEvents) {
-	listenerEl.shouldListenEvents -= 1;
+        listenerEl.shouldListenEvents -= 1;
       }
       console.log('disable listening event by', id);
     }
   },
-  eventDeliveryEnabled: function(id, distributor) {
+  eventDeliveryEnabled: function (id, distributor) {
     const listenerEl = this.objects.get(id)?.data?.el;
     if (checkListenerList(listenerEl, distributor)) {
       if (distributor.listenersList[id]) {
-	return true;
+        return true;
       }
     }
     return false;
   },
-  eventDeliveryOneLocation: function(id, distributor) {
+  eventDeliveryOneLocation: function (id, distributor) {
     const idList = this.list();
     if (!idList.includes(id)) {
       console.error('The specified id does not exist in the registry:', id);
@@ -65,15 +65,15 @@ AFRAME.registerComponent('robot-registry', {
     // console.log('*#*# data:', data);
     const listenerEl = this.objects.get(id)?.data?.el;
     if (checkListenerList(listenerEl, distributor)) {
-      Object.keys(distributor.listenersList).forEach(key => 
-	this.disableEventDelivery(key, distributor));
+      Object.keys(distributor.listenersList).forEach(key =>
+        this.disableEventDelivery(key, distributor));
       this.enableEventDelivery(id, distributor);
     }
   },
-  list: function() {
+  list: function () {
     return Array.from(this.objects.keys());
   },
-  remove: function(id) {
+  remove: function (id) {
     this.objects.delete(id);
   },
 });
@@ -86,36 +86,36 @@ AFRAME.registerComponent('event-distributor', {
       // const robotRegistry = document.getElementById('robot-registry');
       // const robotRegistryComp = robotRegistry?.components['robot-registry'];
       if (!robotRegistryComp) {
-	console.error('robot-registry component not found!');
-	return;
+        console.error('robot-registry component not found!');
+        return;
       }
-      this.distributionFunc =  (evt) => {
-	const detail = evt.detail ? evt.detail : {};
-	robotRegistryComp.list().forEach(id => {
-	  // console.log('*** event distributor: ', evtName, ' to ', id, 
-	  // 	      ' enabled=', robotRegistryComp.eventDeliveryEnabled(id));
-	  const listenerEl = this.el?.listenersList[id];
-	  if (listenerEl) {
-	    detail.originalTarget = evt.target;
-	    listenerEl.emit(evt.type, detail, true);
-	  }
-	});
+      this.distributionFunc = (evt) => {
+        const detail = evt.detail ? evt.detail : {};
+        robotRegistryComp.list().forEach(id => {
+          // console.log('*** event distributor: ', evtName, ' to ', id, 
+          // 	      ' enabled=', robotRegistryComp.eventDeliveryEnabled(id));
+          const listenerEl = this.el?.listenersList[id];
+          if (listenerEl) {
+            detail.originalTarget = evt.target;
+            listenerEl.emit(evt.type, detail, true);
+          }
+        });
       };
 
       ['thumbmenu-select',
-       'triggerdown', 'triggerup', 'gripdown', 'gripup',
-       'abuttondown', 'abuttonup', 'bbuttondown', 'bbuttonup',
-       'thumbstickmoved', 'thumbstickdown', 'thumbstickup',
+        'triggerdown', 'triggerup', 'gripdown', 'gripup',
+        'abuttondown', 'abuttonup', 'bbuttondown', 'bbuttonup',
+        'thumbstickmoved', 'thumbstickdown', 'thumbstickup',
       ].forEach(evtName => {
-	this.el.addEventListener(evtName, this.distributionFunc);
+        this.el.addEventListener(evtName, this.distributionFunc);
       });
     };
     if (this.el.sceneEl.hasLoaded) {
       distributorSetup();
     } else {
       const distributorSetupHandler = () => {
-	distributorSetup();
-	this.el.sceneEl.removeEventListener('loaded', distributorSetupHandler);
+        distributorSetup();
+        this.el.sceneEl.removeEventListener('loaded', distributorSetupHandler);
       }
       this.el.sceneEl.addEventListener('loaded', distributorSetupHandler);
     }
@@ -126,9 +126,9 @@ AFRAME.registerComponent('event-distributor', {
       return;
     }
     ['thumbmenu-select',
-     'triggerdown', 'triggerup', 'gripdown', 'gripup',
-     'abuttondown', 'abuttonup', 'bbuttondown', 'bbuttonup',
-     'thumbstickmoved', 'thumbstickdown', 'thumbstickup',
+      'triggerdown', 'triggerup', 'gripdown', 'gripup',
+      'abuttondown', 'abuttonup', 'bbuttondown', 'bbuttonup',
+      'thumbstickmoved', 'thumbstickdown', 'thumbstickup',
     ].forEach(evtName => {
       this.el.removeEventListener(evtName, this.distributionFunc);
     });
@@ -138,26 +138,26 @@ AFRAME.registerComponent('event-distributor', {
 
 AFRAME.registerComponent('target-selector', {
   schema: {
-    event: { default: 'thumbmenu-select'}
+    event: { default: 'thumbmenu-select' }
   },
   init: function () {
     this.el.addEventListener(this.data.event, (evt) => {
       let distributorEl = null;
       if (this.el.getAttribute('event-distributor')) {
-	distributorEl = this.el;
+        distributorEl = this.el;
       } else if (this.el.sceneEl.getAttribute('event-distributor')) {
-	distributorEl = this.el.sceneEl;
+        distributorEl = this.el.sceneEl;
       }
       const robotRegistryComp = this.el.sceneEl.hasLoaded &&
-	    this.el.sceneEl.robotRegistryComp;
+        this.el.sceneEl.robotRegistryComp;
       const menuText = evt.detail?.texts[evt.detail?.index];
       if (distributorEl && robotRegistryComp && menuText) {
-	for (const id of robotRegistryComp.list()) {
-	  if (menuText === id) {
-	    robotRegistryComp.eventDeliveryOneLocation(id, distributorEl);
-	    break;
-	  }
-	}
+        for (const id of robotRegistryComp.list()) {
+          if (menuText === id) {
+            robotRegistryComp.eventDeliveryOneLocation(id, distributorEl);
+            break;
+          }
+        }
       }
     });
   }
@@ -168,18 +168,18 @@ function checkListenerList(listener, distributor) {
   if (listener?.isEntity && distributor.hasLoaded) {
     if (!listener?.shouldListenEvents) listener.shouldListenEvents = 0;
     if (Object.prototype.toString.call(distributor?.listenersList)
-	=== '[object Object]') {
+      === '[object Object]') {
       if (// listener.hasLoaded &&
-	Number.isInteger(listener.shouldListenEvents)) {
-	return true;
+        Number.isInteger(listener.shouldListenEvents)) {
+        return true;
       } else {
-	console.error('el.shoudListenEvents must be INTEGER. ',
-		      listener?.shouldListenEvents);
-	return false;
+        console.error('el.shoudListenEvents must be INTEGER. ',
+          listener?.shouldListenEvents);
+        return false;
       }
     } else {
       console.error('distributor.listenersList must be a plain boject. :',
-		    distributor?.listenersList);
+        distributor?.listenersList);
       return false;
     }
   } else {
@@ -195,11 +195,11 @@ AFRAME.registerComponent('default-target', {
   init: function () {
     console.log("Set default-target");
     // ロボットの読み込みが終わったら！
-      this.el.addEventListener('ik-worker-start', () => {
-        const robotRegistryComp = this.el.sceneEl.robotRegistryComp;
-        robotRegistryComp?.eventDeliveryOneLocation(this.el.id); // デフォルトでここだけに！
-        console.log('### default-target: enabled event delivery for id:', this.el.id);
-      });
+    this.el.addEventListener('ik-worker-start', () => {
+      const robotRegistryComp = this.el.sceneEl.robotRegistryComp;
+      robotRegistryComp?.eventDeliveryOneLocation(this.el.id); // デフォルトでここだけに！
+      console.log('### default-target: enabled event delivery for id:', this.el.id);
+    });
 
   }
 });
